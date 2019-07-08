cf8a63ef2619ffdb92c06c3e3d35a3be193a7d8b
Bret Beheim <beheim@gmail.com>
2019-07-08 16:38:49 -0400

    added more tests, and a .gitignore file

--- tests/testthat/test_simple_forms.r
+++ tests/testthat/test_simple_forms.r
@@ -1,19 +1,19 @@
 
-forms <- list.files("./tutorial_forms", pattern = "\\.yaml$", full.names = TRUE)
+forms <- list.files("./simple", pattern = "\\.yaml$", full.names = TRUE)
 
-test_that("standard tutorial forms load", {
+test_that("simple forms load", {
   for (i in 1:length(forms)) {
     expect_silent(read_yamlform(forms[i]))
   }
 })
 
-test_that("standard tutorial forms converts to xlsx", {
+test_that("simple forms converts to xlsx", {
   for (i in 1:length(forms)) {
     expect_output(convert_yamlform(forms[i]))
   }
 })
 
-test_that("standard tutorial forms converts to xml using pyxform", {
+test_that("simple forms converts to xml using pyxform", {
   for (i in 1:length(forms)) {
     expect_output(convert_yamlform(forms[i]), output = "xml")
   }
